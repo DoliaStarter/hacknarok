8c556e26174498b7046a0a243a2a782cef9344ef
vvlovsky <slvtrshkv@gmail.com>
2020-08-29 22:41:24 +0200

    url fix

--- backend/src/api/quests/urls.py
+++ backend/src/api/quests/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.quest, name='questList'),
     path('search', views.quest_search, name='questSearch'),
     path('<int:id>/', views.get_quest_model, name='quest'),
-    path('<int:id>/answers', views.get_base_point_model, name='basePoint'),
+    path('<int:id>/answers', views.get_quest_point_model, name='basePoint'),
     path('<int:id>/start', views.start_session, name='start_session'),
 
 ]
