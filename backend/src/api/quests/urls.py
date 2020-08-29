13f28bb6fa4cf30db9e481e25f0f9e01f81bd2c9
dimonium_239 <dimon041220000@gmail.com>
2020-08-29 22:22:06 +0200

    Bug fix and add DB content

--- backend/src/api/quests/urls.py
+++ backend/src/api/quests/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.quest, name='questList'),
-    path('<int:id>/', views.get_quest_model, name='quest'),
-    path('<int:id>/answers', views.get_base_point_model, name='basePoint'),
+    path('<int:id>', views.get_quest_model, name='quest'),
+    #path('<int:id>/answers', views.get_base_point_model, name='basePoint'),
 ]
