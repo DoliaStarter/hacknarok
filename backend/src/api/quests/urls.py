35de74af01eb6abb21f8afcb4d2d3d514a17fb2e
vvlovsky <slvtrshkv@gmail.com>
2020-08-29 22:35:31 +0200

    url added

--- backend/src/api/quests/urls.py
+++ backend/src/api/quests/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('search', views.quest_search, name='questSearch'),
     path('<int:id>/', views.get_quest_model, name='quest'),
     path('<int:id>/answers', views.get_base_point_model, name='basePoint'),
+    path('<int:id>/start', views.start_session, name='start_session'),
+
 ]
