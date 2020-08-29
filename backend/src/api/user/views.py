abb2d2371bf4ce50e75a8aa0a507319d78354478
dimonium_239 <dimon041220000@gmail.com>
2020-08-29 19:06:24 +0200

    Login/Registration changed

--- backend/src/api/user/views.py
+++ backend/src/api/user/views.py
@@ -12,44 +12,22 @@ def getAciveQuests(request, id):
         'quests': [session.quest.title for session in sessions]
     })
 
-# TODO Tight with  forms and redirect to wright page
 def register(request):
-    context = {'form': RegistrationForm(),
-               'action': request.build_absolute_uri()}
-    print(context)
     if request.method == 'POST':
-        user_form = RegistrationForm(data=request.POST)
-        if user_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            user_profile = UserProfile()
-            user_profile.user = user
-            user_profile.save()
-            login(request,user)
-            return HttpResponseRedirect('user_cabinet/')
+        login=request.POST['login']
+        password=request.POST['password'] 
+        if (login not in [user.login for user in User.objects.all()]):
+            newUser = User(login,password)
+            newUser.save()
         else:
-            print(user_form.errors)
-    return render(request, 'main_page/authentification.html', context)
+            raise "Here is such user"
 
 
 def sign_in(request):
-    failed = False
     if request.method == 'POST':
-        form = SignInForm(request.POST)
-        if request.method == 'POST':
-            username = request.POST['username']
-            password = request.POST['password']
-            # Use Django's machinery to attempt to see if the username/password
-            # combination is valid - a User object is returned if it is.
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return HttpResponseRedirect('user_cabinet/')
-            else:
-                return HttpResponse("Invalid login details supplied.")
-    context = {'form': SignInForm(),
-               'action': request.build_absolute_uri(),
-               'failed': failed}
-    return render(request, 'main_page/authentification.html', context)
+        login=request.POST['login']
+        password=request.POST['password'] 
+        if (login in [user.login for user in User.objects.all()] and \
+            password in [user.password for user in User.objects.all()]):
+            return True
+        return False
